228de6e4f36c5f07c0f4963a91a4ddb1d9b3ee28
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-04-18 10:39:20 -0700

    Archive previous daily problem; Add current daily problem.

--- leetcode/daily_problem.py
+++ leetcode/daily_problem.py
@@ -1,76 +1,115 @@
 '''
 ========================================
-1431. Kids With the Greatest Number of Candies
+1768. Merge Strings Alternately
 ========================================
-There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 
-Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
-
-Note that multiple kids can have the greatest number of candies.
+Return the merged string.
 
 Example 1:
-Input: candies = [2,3,5,1,3], extraCandies = 3
-Output: [true,true,true,false,true] 
-Explanation: If you give all extraCandies to:
-- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
-- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
-- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
-- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
-- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
 
 Example 2:
-Input: candies = [4,2,1,1,2], extraCandies = 1
-Output: [true,false,false,false,false] 
-Explanation: There is only 1 extra candy.
-Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
 
 Example 3:
-Input: candies = [12,1,12], extraCandies = 10
-Output: [true,false,true]
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
  
 Constraints:
-n == candies.length
-2 <= n <= 100
-1 <= candies[i] <= 100
-1 <= extraCandies <= 50
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
 
 ========================================
 Initial Thoughts
-- This seems fairly straightforward...we determine the initial max number of candies out of all the kids, and then simulate giving each kid extraCandies to determine if the kid will have a greater or equal amount of candies as the initial max number of candies
+- We could take characters from word2 and insert them into word1 at the appropriate indices...but modifying a string costs O(n), so it wouldn't be efficient
 
 ========================================
-Simulation
-- Use an integer to store the initial max number of candies -> max_candies
-- Use an array for the output -> results
-- Iterate candies -> kid, count
-    -- Determine if the sum of count and extraCandies is greater than or equal to max_candies -> Update results[kid]
-- Return results
+Two Pointers
+- The intuition here is to use pointers to traverse each input string individually and add the characters to the merged string at each step
+- Since strings are immutable in Python, we can use an array to keep track of the characters until the end where we can join them to efficiently create the output
+
+Algo
+- Use an array to store the characters of the merged string -> builder
+- Use two pointers to traverse the input strings -> pos1, pos2
+- Loop until both pointers reach the end of their respective word
+    - If pos1 is not at the end -> Add char at pos1 to builder
+    - If pos2 is not at the end -> Add char at pos2 to builder
+- Join the characters in builder -> Return
 
 Performance
-- Let n be the number of kids
-Time ->
-    -- Determining max_candies costs O(n)
-    -- Iterating candies costs O(n)
-    -- Updating results costs O(1)
-    -- Overall -> O(n)
-Space ->
-    -- Since results array is required for the output, it does not contribute to the space complexity
-    -- Overall -> O(1)
+- Let m be the length of word1
+- Let n be the length of word2
+- Time
+    -- Using pointer variables costs O(1)
+    -- Traversing the input strings costs O(max(m, n))
+    -- Joining the builder array costs O(m + n)
+    -- Overall -> O(m + n)
+- Space
+    -- Typically the space used for the output does not count toward the space complexity, but we are not returning an array for this problem, therefore the space for the builder array should be considered...and the cost is O(m + n)
+    -- Overall -> O(m + n)
+
+========================================
+One Pointer
+- This approach doesn't provide an improvement to time or space, but it can be considered cleaner and worth knowing
+- Rather than using a pointer to traverse each input string, we can modify the Two Pointers approach to use a single pointer and take advantage of the checks already in-place for adding characters
+
+Algo
+- Use an array to store the characters of the merged string -> builder
+- Use a for loop to iterate from 0 to the end of the longest input string -> pos
+    - If pos is not at the end of word1 -> Add word1[pos] to builder
+    - If pos is not at the end of word2 -> Add word2[pos] to builder
+- Join the characters in builder -> Return
+
+Performance
+- As mentioned before, the time and space does not change from the Two Pointers approach
+    -- Time -> O(m + n)
+    -- Space -> O(m + n)
 
 ========================================
 '''
 
-from typing import List
-
 class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        ''' ===== Simulation ===== '''
-        KIDS = len(candies)
-        max_candies = max(candies)
-        results = [False for _ in range(KIDS)]
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ''' ===== Two Pointers ===== '''
+        # builder = []
+        # pos1 = 0
+        # pos2 = 0
+        
+        # while pos1 < len(word1) or pos2 < len(word2):
+        #     if pos1 < len(word1):
+        #         builder.append(word1[pos1])
+        #         pos1 += 1
 
-        for kid, count in enumerate(candies):
-            if count + extraCandies >= max_candies:
-                results[kid] = True
+        #     if pos2 < len(word2):
+        #         builder.append(word2[pos2])
+        #         pos2 += 1
 
-        return results
+        # return ''.join(builder)
+
+        ''' ===== One Pointer ===== '''
+        builder = []
+        end = max(len(word1), len(word2))
+
+        for pos in range(end):
+            if pos < len(word1):
+                builder.append(word1[pos])
+
+            if pos < len(word2):
+                builder.append(word2[pos])
+
+        return ''.join(builder)
